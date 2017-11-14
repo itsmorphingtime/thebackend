b298c8786ea3b7a63f393e005c1dabd5c3bb8afa
MikeD <mikeddolha@gmail.com>
2017-11-14 23:21:50 +0200

    enhance and fix getting attributes

--- routes/api.js
+++ routes/api.js
@@ -20,19 +20,11 @@ router.get('/:id', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+	var myAttr = getProperAttributes(req.body);
 	db.createUser({
 			name: req.body.name || randomWords(3).join("-"),
 			score: parseInt(req.body.score) || 0,
-			attributes: {
-				amountOfBlobs: parseInt(req.body.attributes.amountOfBlobs) || 0,
-				upwardThrust: parseInt(req.body.attributes.upwardThrust) || 0,
-				leftThrust: parseInt(req.body.attributes.leftThrust) || 0,
-				rightThrust: parseInt(req.body.attributes.rightThrust) || 0,
-				downwardThrust: parseInt(req.body.attributes.downwardThrust) || 0,
-				anticlockwiseTorque: parseInt(req.body.attributes.anticlockwiseTorque) || 0,
-				clockwiseTorque: parseInt(req.body.attributes.clockwiseTorque) || 0,
-				timeStamp: req.body.timeStamp
-			}
+			attributes: myAttr
 		})
 		.then(function(data) {
 			res.json(data);
@@ -43,27 +35,45 @@ router.put('/', function(req, res, next) {
 	if (req.rawBody) {
 		req.body = JSON.parse(req.rawBody);
 	}
+	var myAttr = getProperAttributes(req.body);
 	db.updateUser({
 			id: req.body._id,
 			name: req.body.name,
 			score: parseInt(req.body.score) || 0,
 			$push: {
-				attributes: {
-					amountOfBlobs: parseInt(req.body.attributes.amountOfBlobs) || 0,
-					upwardThrust: parseInt(req.body.attributes.upwardThrust) || 0,
-					leftThrust: parseInt(req.body.attributes.leftThrust) || 0,
-					rightThrust: parseInt(req.body.attributes.rightThrust) || 0,
-					downwardThrust: parseInt(req.body.attributes.downwardThrust) || 0,
-					anticlockwiseTorque: parseInt(req.body.attributes.anticlockwiseTorque) || 0,
-					clockwiseTorque: parseInt(req.body.attributes.clockwiseTorque) || 0,
-					timeStamp: req.body.timeStamp
-				}
+				attributes: myAttr
 			}
 		})
 		.then(function(data) {
 			res.json(data);
 		});
 });
+
+function getProperAttributes(body) {
+	if (body.attributes) {
+		return {
+			amountOfBlobs: parseInt(body.attributes.amountOfBlobs) || 0,
+			upwardThrust: parseInt(body.attributes.upwardThrust) || 0,
+			leftThrust: parseInt(body.attributes.leftThrust) || 0,
+			rightThrust: parseInt(body.attributes.rightThrust) || 0,
+			downwardThrust: parseInt(body.attributes.downwardThrust) || 0,
+			anticlockwiseTorque: parseInt(body.attributes.anticlockwiseTorque) || 0,
+			clockwiseTorque: parseInt(body.attributes.clockwiseTorque) || 0,
+			timeStamp: body.attributes.timeStamp
+		};
+	} else {
+		return {
+			amountOfBlobs: parseInt(body.amountOfBlobs) || 0,
+			upwardThrust: parseInt(body.upwardThrust) || 0,
+			leftThrust: parseInt(body.leftThrust) || 0,
+			rightThrust: parseInt(body.rightThrust) || 0,
+			downwardThrust: parseInt(body.downwardThrust) || 0,
+			anticlockwiseTorque: parseInt(body.anticlockwiseTorque) || 0,
+			clockwiseTorque: parseInt(body.clockwiseTorque) || 0,
+			timeStamp: body.timeStamp
+		};
+	}
+}
 
 router.delete('/:id', function(req, res, next) {
 	db.removeUserById(req.params.id)
