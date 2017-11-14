489dadb3b32e50278a889a441e712caf4c44f8d7
MikeD <mikeddolha@gmail.com>
2017-11-14 23:15:24 +0200

    oh my god i realized what was wrong all laon

--- routes/api.js
+++ routes/api.js
@@ -24,13 +24,13 @@ router.post('/', function(req, res, next) {
 			name: req.body.name || randomWords(3).join("-"),
 			score: parseInt(req.body.score) || 0,
 			attributes: {
-				amountOfBlobs: parseInt(req.body.amountOfBlobs) || 0,
-				upwardThrust: parseInt(req.body.upwardThrust) || 0,
-				leftThrust: parseInt(req.body.leftThrust) || 0,
-				rightThrust: parseInt(req.body.rightThrust) || 0,
-				downwardThrust: parseInt(req.body.downwardThrust) || 0,
-				anticlockwiseTorque: parseInt(req.body.anticlockwiseTorque) || 0,
-				clockwiseTorque: parseInt(req.body.clockwiseTorque) || 0,
+				amountOfBlobs: parseInt(req.body.attributes.amountOfBlobs) || 0,
+				upwardThrust: parseInt(req.body.attributes.upwardThrust) || 0,
+				leftThrust: parseInt(req.body.attributes.leftThrust) || 0,
+				rightThrust: parseInt(req.body.attributes.rightThrust) || 0,
+				downwardThrust: parseInt(req.body.attributes.downwardThrust) || 0,
+				anticlockwiseTorque: parseInt(req.body.attributes.anticlockwiseTorque) || 0,
+				clockwiseTorque: parseInt(req.body.attributes.clockwiseTorque) || 0,
 				timeStamp: req.body.timeStamp
 			}
 		})
@@ -43,24 +43,21 @@ router.put('/', function(req, res, next) {
 	if (req.rawBody) {
 		req.body = JSON.parse(req.rawBody);
 	}
-	console.log(JSON.stringify(req.body));
-	var myAttr = {
-		amountOfBlobs: req.body.amountOfBlobs,
-		upwardThrust: req.body.upwardThrust,
-		leftThrust: req.body.leftThrust,
-		rightThrust: req.body.rightThrust,
-		downwardThrust: req.body.downwardThrust,
-		anticlockwiseTorque: req.body.anticlockwiseTorque,
-		clockwiseTorque: req.body.clockwiseTorque,
-		timeStamp: req.body.timeStamp
-	};
-	console.log(JSON.stringify(myAttr));
 	db.updateUser({
 			id: req.body._id,
 			name: req.body.name,
-			score: req.body.score,
+			score: parseInt(req.body.score) || 0,
 			$push: {
-				attributes: myAttr
+				attributes: {
+					amountOfBlobs: parseInt(req.body.attributes.amountOfBlobs) || 0,
+					upwardThrust: parseInt(req.body.attributes.upwardThrust) || 0,
+					leftThrust: parseInt(req.body.attributes.leftThrust) || 0,
+					rightThrust: parseInt(req.body.attributes.rightThrust) || 0,
+					downwardThrust: parseInt(req.body.attributes.downwardThrust) || 0,
+					anticlockwiseTorque: parseInt(req.body.attributes.anticlockwiseTorque) || 0,
+					clockwiseTorque: parseInt(req.body.attributes.clockwiseTorque) || 0,
+					timeStamp: req.body.timeStamp
+				}
 			}
 		})
 		.then(function(data) {
