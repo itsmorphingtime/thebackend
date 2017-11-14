39a7e9b2504dcedf78392d3dc55edcee925ddecb
MikeD <mikeddolha@gmail.com>
2017-11-15 00:21:27 +0200

    improve templates and ordering of games

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var R = require('ramda');
 var db = require('../database/database');
 db.connect();
 
@@ -7,6 +8,14 @@ db.connect();
 router.get('/', function(req, res, next) {
 	var users = db.getAllUsers()
 		.then(function(users) {
+			users = R.sort(function(a, b) {
+				return b.timeStamp - a.timeStamp;
+			}, R.map(function(user) {
+				user.timeStamp = R.sort(function(a, b) {
+					return b.timeStamp - a.timeStamp;
+				}, user.attributes)[0].timeStamp;
+				return user;
+			}, users));
 			res.render('index', {
 				users: users
 			});
